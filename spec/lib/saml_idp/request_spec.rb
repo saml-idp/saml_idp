66714394fcbe5023a86ed823e73c05fe541be229
Francois Deschenes <fdeschenes@me.com>
2015-08-21 15:48:45 -0700

    Added some tests for the functionality I added.

--- spec/lib/saml_idp/request_spec.rb
+++ spec/lib/saml_idp/request_spec.rb
@@ -1,32 +1,46 @@
 require 'spec_helper'
 module SamlIdp
   describe Request do
-    let(:raw_request) { "<samlp:AuthnRequest AssertionConsumerServiceURL='http://localhost:3000/saml/consume' Destination='http://localhost:1337/saml/auth' ID='_af43d1a0-e111-0130-661a-3c0754403fdb' IssueInstant='2013-08-06T22:01:35Z' Version='2.0' xmlns:samlp='urn:oasis:names:tc:SAML:2.0:protocol'><saml:Issuer xmlns:saml='urn:oasis:names:tc:SAML:2.0:assertion'>localhost:3000</saml:Issuer><samlp:NameIDPolicy AllowCreate='true' Format='urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress' xmlns:samlp='urn:oasis:names:tc:SAML:2.0:protocol'/></samlp:AuthnRequest>" }
-    subject { described_class.new raw_request }
+    describe "with an inflated and valid SAML request" do
+      let(:raw_request) { "<samlp:AuthnRequest AssertionConsumerServiceURL='http://localhost:3000/saml/consume' Destination='http://localhost:1337/saml/auth' ID='_af43d1a0-e111-0130-661a-3c0754403fdb' IssueInstant='2013-08-06T22:01:35Z' Version='2.0' xmlns:samlp='urn:oasis:names:tc:SAML:2.0:protocol'><saml:Issuer xmlns:saml='urn:oasis:names:tc:SAML:2.0:assertion'>localhost:3000</saml:Issuer><samlp:NameIDPolicy AllowCreate='true' Format='urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress' xmlns:samlp='urn:oasis:names:tc:SAML:2.0:protocol'/></samlp:AuthnRequest>" }
+      subject { described_class.new raw_request }
 
-    it "has a valid request_id" do
-      subject.request_id.should == "_af43d1a0-e111-0130-661a-3c0754403fdb"
+      it "has a valid request_id" do
+        subject.request_id.should == "_af43d1a0-e111-0130-661a-3c0754403fdb"
+      end
+
+      it "has a valid acs_url" do
+        subject.acs_url.should == "http://localhost:3000/saml/consume"
+      end
+
+      it "has a valid service_provider" do
+        subject.service_provider.should be_a ServiceProvider
+      end
+
+      it "has a valid service_provider" do
+        subject.service_provider.should be_truthy
+      end
+
+      it "has a valid issuer" do
+        subject.issuer.should == "localhost:3000"
+      end
+
+      it "has a valid valid_signature" do
+        subject.valid_signature?.should be_truthy
+      end
     end
 
-    it "has a valid acs_url" do
-      subject.acs_url.should == "http://localhost:3000/saml/consume"
+    describe "with an inflated but invalid SAML request" do
+      it "returns an empty string when " do
+        described_class.new "<samlp:AuthnRequest></samlp:AuthnRequest>"
+        subject.acs_url == ""
+      end
     end
 
-    it "has a valid service_provider" do
-      subject.service_provider.should be_a ServiceProvider
+    describe "with an improperly deflated SAML request" do
+      it "rescues Zlib::BufError exceptions" do
+        expect { described_class.from_deflated_request nil }.to_not raise_error
+      end
     end
-
-    it "has a valid service_provider" do
-      subject.service_provider.should be_truthy
-    end
-
-    it "has a valid issuer" do
-      subject.issuer.should == "localhost:3000"
-    end
-
-    it "has a valid valid_signature" do
-      subject.valid_signature?.should be_truthy
-    end
-
   end
 end
