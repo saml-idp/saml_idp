cd71cd2987027c145579dec3bc57efc98943cf5d
Christopher Foo <christopher.t.foo@gmail.com>
2017-06-21 11:57:28 -1000

    Refactor change into parent class.

--- lib/saml_idp/logout_builder.rb
+++ lib/saml_idp/logout_builder.rb
@@ -3,13 +3,25 @@ module SamlIdp
   class LogoutBuilder
     include Signable
 
+    attr_accessor :response_id
+    attr_accessor :issuer_uri
+    attr_accessor :saml_slo_url
+    attr_accessor :algorithm
+
+    def initialize(response_id, issuer_uri, saml_slo_url, algorithm)
+      self.response_id = response_id
+      self.issuer_uri = issuer_uri
+      self.saml_slo_url = saml_slo_url
+      self.algorithm = algorithm
+    end
+
     # this is an abstract base class.
     def build
       raise "#{self.class} must implement build method"
     end
 
     def reference_id
-      UUID.generate
+      self.response_id
     end
 
     def encoded
--- lib/saml_idp/logout_response_builder.rb
+++ lib/saml_idp/logout_response_builder.rb
@@ -1,22 +1,11 @@
 require 'saml_idp/logout_builder'
 module SamlIdp
   class LogoutResponseBuilder < LogoutBuilder
-    attr_accessor :response_id
-    attr_accessor :issuer_uri
-    attr_accessor :saml_slo_url
     attr_accessor :saml_request_id
-    attr_accessor :algorithm
 
     def initialize(response_id, issuer_uri, saml_slo_url, saml_request_id, algorithm)
-      self.response_id = response_id
-      self.issuer_uri = issuer_uri
-      self.saml_slo_url = saml_slo_url
+      super(response_id, issuer_uri, saml_slo_url, algorithm)
       self.saml_request_id = saml_request_id
-      self.algorithm = algorithm
-    end
-
-    def reference_id
-      self.response_id
     end
 
     def build
