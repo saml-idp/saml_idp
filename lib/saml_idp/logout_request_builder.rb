a87c8100b040f576cef79ffbf264cf157e9797c9
Christopher Foo <Christopher.T.Foo@gmail.com>
2017-06-21 12:05:43 -1000

    Fix invalid LogoutRequest (#68)
    * URI of LogoutRequest must match ID.
    * Digest should be base64 encoded, not a hexstring.
    The default digest method properly creates the
    digest so fall back to using that method.
    * Fix reference_id for LogoutResponse too.
    * Refactor change into parent class.

--- lib/saml_idp/logout_request_builder.rb
+++ lib/saml_idp/logout_request_builder.rb
@@ -1,18 +1,11 @@
 require 'saml_idp/logout_builder'
 module SamlIdp
   class LogoutRequestBuilder < LogoutBuilder
-    attr_accessor :response_id
-    attr_accessor :issuer_uri
-    attr_accessor :saml_slo_url
     attr_accessor :name_id
-    attr_accessor :algorithm
 
     def initialize(response_id, issuer_uri, saml_slo_url, name_id, algorithm)
-      self.response_id = response_id
-      self.issuer_uri = issuer_uri
-      self.saml_slo_url = saml_slo_url
+      super(response_id, issuer_uri, saml_slo_url, algorithm)
       self.name_id = name_id
-      self.algorithm = algorithm
     end
 
     def build
