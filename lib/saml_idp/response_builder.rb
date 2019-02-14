bcb496f24d4dde1f9e89c5e3a2993af18a56a33f
otsogbadrakhchinzorig <tsogbadrakh.chinzorig@globalsign.com>
2019-02-14 15:46:04 +0900

    Signed SAML response to SP
    Signed message 
    
    Some SP required to do sign SAML response message it self.
    Follow the origin coding style
    
    
    Signed message opts
    
    
    signed was the instance method 
    
    
    Identify incoming metadata
    
    
    Small test for Entity ID
    
    
    Revert "Identify incoming metadata"
    
    This reverts commit 1d859c1842a12662e585b4a4584e504d8a5eae4f.
    
    Signed message (document)
    
    ID should be same for sign and document
    Merge branch 'entity_id_for_incoming_metadata' into updates_for_saml_idp
    
    
    Identify incoming metadata
    
    
    Small test for Entity ID
    
    
    Name space should be defined
    
    
    Merge branch 'entity_id_for_incoming_metadata' into updates_for_saml_idp
    Signed response
    
    
    Merge branch 'updates_for_saml_idp' into signed_response_message
    
    
    Signed response
    
    This reverts commit cba7e02255dd82ae5f11b15654bb541f75655a3d.
    
    Signed response
    
    
    Test for Signature part

--- lib/saml_idp/response_builder.rb
+++ lib/saml_idp/response_builder.rb
@@ -1,32 +1,43 @@
 require 'builder'
+require 'saml_idp/algorithmable'
+require 'saml_idp/signable'
 module SamlIdp
   class ResponseBuilder
+    include Algorithmable
+    include Signable
     attr_accessor :response_id
     attr_accessor :issuer_uri
     attr_accessor :saml_acs_url
     attr_accessor :saml_request_id
     attr_accessor :assertion_and_signature
+    attr_accessor :raw_algorithm
 
-    def initialize(response_id, issuer_uri, saml_acs_url, saml_request_id, assertion_and_signature)
+    def initialize(response_id, issuer_uri, saml_acs_url, saml_request_id, assertion_and_signature, raw_algorithm)
       self.response_id = response_id
       self.issuer_uri = issuer_uri
       self.saml_acs_url = saml_acs_url
       self.saml_request_id = saml_request_id
       self.assertion_and_signature = assertion_and_signature
+      self.raw_algorithm = raw_algorithm
     end
 
-    def encoded
-      @encoded ||= encode
+    def encoded(signed_message: false)
+      @encoded ||= signed_message ? encode_signed_message : encode_raw_message
     end
 
     def raw
       build
     end
 
-    def encode
+    def encode_raw_message
       Base64.strict_encode64(raw)
     end
-    private :encode
+    private :encode_raw_message
+
+    def encode_signed_message
+      Base64.strict_encode64(signed)
+    end
+    private :encode_signed_message
 
     def build
       resp_options = {}
@@ -41,6 +52,7 @@ module SamlIdp
       builder = Builder::XmlMarkup.new
       builder.tag! "samlp:Response", resp_options do |response|
           response.Issuer issuer_uri, xmlns: Saml::XML::Namespaces::ASSERTION
+          sign response
           response.tag! "samlp:Status" do |status|
             status.tag! "samlp:StatusCode", Value: Saml::XML::Namespaces::Statuses::SUCCESS
           end
@@ -52,6 +64,7 @@ module SamlIdp
     def response_id_string
       "_#{response_id}"
     end
+    alias_method :reference_id, :response_id
     private :response_id_string
 
     def now_iso
