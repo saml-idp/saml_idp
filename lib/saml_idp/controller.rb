ee0016d09eade58b67e0928eaa708e467255ae11
Mathieu Jobin <majobin@mdsol.com>
2020-12-14 13:55:02 +0900

    Remove conditionals about Rails 4 or earlier

--- lib/saml_idp/controller.rb
+++ lib/saml_idp/controller.rb
@@ -37,11 +37,7 @@ module SamlIdp
       decode_request(raw_saml_request)
       return true if valid_saml_request?
       if defined?(::Rails)
-        if Rails::VERSION::MAJOR >= 4
-          head :forbidden
-        else
-          render nothing: true, status: :forbidden
-        end
+        head :forbidden
       end
       false
     end
--- spec/rails_app/app/controllers/saml_controller.rb
+++ spec/rails_app/app/controllers/saml_controller.rb
@@ -2,11 +2,7 @@ class SamlController < ApplicationController
 
   def consume
     response = OneLogin::RubySaml::Response.new(params[:SAMLResponse])
-    if Gem::Requirement.new('< 4.1') =~ Gem::Version.new(Rails.version)
-      render :text => response.name_id
-    else
-      render :plain => response.name_id
-    end
+    render :plain => response.name_id
   end
 
 end
