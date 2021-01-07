8e8675badfdf18390fd8f9f1fc8bda0892269a4d
Zogoo <chtsogbadrakh@gmail.com>
2021-01-07 13:23:21 +0900

    Update rails app test

--- spec/rails_app/app/controllers/saml_idp_controller.rb
+++ spec/rails_app/app/controllers/saml_idp_controller.rb
@@ -1,9 +1,48 @@
-class SamlIdpController < SamlIdp::IdpController
-  def idp_authenticate(email, password)
-    { :email => email }
-  end
+class SamlIdpController < ApplicationController
+    include SamlIdp::Controller
 
-  def idp_make_saml_response(user)
-    encode_response(user[:email])
-  end
+    before_action :validate_saml_request, only: [:new, :create, :logout]
+
+    def new
+      render template: "saml_idp/idp/new"
+    end
+
+    def show
+      render xml: SamlIdp.metadata.signed
+    end
+
+    def create
+      unless params[:email].blank? && params[:password].blank?
+        person = idp_authenticate(params[:email], params[:password])
+        if person.nil?
+          @saml_idp_fail_msg = "Incorrect email or password."
+        else
+          @saml_response = idp_make_saml_response(person)
+          render :template => "saml_idp/idp/saml_post", :layout => false
+          return
+        end
+      end
+      render :template => "saml_idp/idp/new"
+    end
+
+    def logout
+      idp_logout
+      @saml_response = idp_make_saml_response(nil)
+      render :template => "saml_idp/idp/saml_post", :layout => false
+    end
+
+    def idp_logout
+      raise NotImplementedError
+    end
+    private :idp_logout
+
+    def idp_authenticate(email, password)
+      { :email => email }
+    end
+    protected :idp_authenticate
+
+    def idp_make_saml_response(person)
+      encode_response(user[:email])
+    end
+    protected :idp_make_saml_response
 end
