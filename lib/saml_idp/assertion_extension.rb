1571f895a0d74a5512f5b6f821ee1d26bd84a1ea
Chris Born <chris@kolide.co>
2023-10-16 09:27:20 -0700

    Add comment to base class build method

--- lib/saml_idp/assertion_extension.rb
+++ lib/saml_idp/assertion_extension.rb
@@ -11,7 +11,20 @@ module SamlIdp
       self.extension_point = extension_point
     end
 
-    # this is an abstract base class.
+    # This is an abstract base class, an example extension may look like this:
+    #
+    # context.AuthnContextDecl do |builder|
+    #   builder.AuthenticationContextDeclaration xmlns: "urn:my_org:saml:2.0:Device" do |context|
+    #     context.Extension do |ext|
+    #       ext.Device xmlns: "urn:my_org:saml:2.0:ZeroTrust", ID: "f659c992-2b3d-4e2d-a155-6d32161e6754" do |device|
+    #         device.Trust do |trust|
+    #           trust.Data Name: "Managed", Value: true
+    #           trust.Data Name: "Compliant", Value: true
+    #         end
+    #       end
+    #     end
+    #   end
+    # end
     def build(context)
       raise "#{self.class} must implement build method"
     end
